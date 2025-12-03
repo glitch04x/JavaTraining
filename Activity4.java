00b8821144d9490d8b71ebb25c9ab0568e282a36
Jan Exar S. Aspiras <jesaspiras@headoffice.corp.bpi.com.ph>
2025-12-03 18:16:36 +0800

    initial commit

---
+++ Activity4.java
@@ -0,0 +1,36 @@
+package ph.com.bpi.helloworld.isg;
+import java.util.Scanner;
+
+public class HelloworldMain {
+
+	public static void main(String[] args) {
+
+    //    Scanner input = new Scanner(System.in);
+    //    System.out.print("Enter your name: ");
+    //    String name = input.nextLine();
+    //    System.out.println("Hello, " + name + "!");
+
+    //    Scanner input = new Scanner(System.in);
+    //    System.out.print("Enter your age: ");
+    //    String age0 = input.nextLine();
+    //    int age1 = Integer.parseInt(age0);
+    //    double age2 = Double.parseDouble(age0);
+    //    System.out.println("Your age as int " + age1);
+    //    System.out.println("Your age as double " + age2);
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter your age: ");
+        String int0 = input.nextLine();
+        int int1 = Integer.parseInt(int0);
+
+		if (int1 < 18) {
+			System.out.print("Minor");
+		} else if (int1 < 59) {
+			System.out.print("Adult");
+		} else {
+			System.out.print("Senior");
+		}
+   
+	}
+
+}
+
