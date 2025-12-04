899c4cdeea90ed198a3cd2d68e26b5ec3486198a
Jan Exar S. Aspiras <jesaspiras@headoffice.corp.bpi.com.ph>
2025-12-04 16:28:44 +0800

    initial commit

---
+++ Seatwork2.java
@@ -0,0 +1,9 @@
+package ph.com.bpi.training;
+
+public class Seatwork2 {
+	public static void main(String[] args){
+		WhiteBoard wb = new WhiteBoard();
+		int mainvar = wb.addNumbers(5,3);
+		System.out.println("The total is: " + String.valueOf(mainvar));
+	}
+}
---
+++ WhiteBoard.java
@@ -0,0 +1,9 @@
+package ph.com.bpi.training;
+
+public class WhiteBoard {
+	int addNumbers(int addend1, int addend2) {
+		int tempvar = addend1 + addend2;
+		return tempvar;
+	}
+	
+}
