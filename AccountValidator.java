a34310edb9da05312ff64982f40c6456563160b6
Jan Exar S. Aspiras <jesaspiras@headoffice.corp.bpi.com.ph>
2026-01-08 17:58:12 +0800

    initial commit

---
+++ AccountValidator.java
@@ -0,0 +1,63 @@
+package M4_Activity5;
+
+	class InvalidAccountNumberException extends Exception {
+	 public InvalidAccountNumberException(String message) {
+	     super(message);
+	 }
+	}
+	
+	class InvalidAccountFormatException extends RuntimeException {
+	 public InvalidAccountFormatException(String message) {
+	     super(message);
+	 }
+	}
+
+
+
+	public class AccountValidator {
+		public void validateAccountNumber(String accountNumber) throws InvalidAccountNumberException {
+		
+		    if (accountNumber == null) {
+		        throw new NullPointerException("Account number cannot be null");
+		    }
+		
+		    for (char c : accountNumber.toCharArray()) {
+		        if (!Character.isDigit(c)) {
+		            throw new InvalidAccountFormatException("Account number must contain only digits");
+		        }
+		    }
+		
+		    if (accountNumber.length() != 10) {
+		        throw new InvalidAccountNumberException("Account number must be exactly 10 digits");
+		    }
+		
+		    System.out.println("Valid account number: " + accountNumber);
+		}
+		
+		
+		private static void testValidation(String testName, String accountNumber) {
+		    System.out.println(testName);
+		    AccountValidator validator = new AccountValidator();
+		
+		    try {
+		        validator.validateAccountNumber(accountNumber);
+		    } catch (InvalidAccountNumberException e) {
+		        System.out.println("Error: " + e.getMessage());
+		    } catch (InvalidAccountFormatException e) {
+		        System.out.println("Error: " + e.getMessage());
+		    } catch (NullPointerException e) {
+		        System.out.println("Error: " + e.getMessage());
+		    }
+		
+		    System.out.println();
+		}
+		
+		public static void main(String[] args) {
+		    System.out.println("=== Account Number Validation Test ===\n");
+		    testValidation("Test 1: Valid account (1234567890)", "1234567890");
+		    testValidation("Test 2: Too short (123)", "123");
+		    testValidation("Test 3: Contains letters (12345ABC90)", "12345ABC90");
+		    testValidation("Test 4: Contains space (1234 567890)", "1234 567890");
+		    testValidation("Test 5: Null value", null);
+		}
+}
